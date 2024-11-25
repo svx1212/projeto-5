47725a5a5824f9459e9f877682659dfd4fbc9ec5
sergio <sergioefamilha@gmail.com>
2024-11-25 14:12:04 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,25 @@
+criaCartao(
+    'conhecimentos gerais',
+    'O sol é um planeta ou uma estrela?',
+    'O Sol é uma estrela anã amarela que se encontra no centro do sistema solar, e em um braço externo de nossa Galáxia, a Via Láctea. Sua massa constitui à 99,8% da massa do sistema solar. É uma bola de gás mantida unida por sua própria gravidade.'
+
+
+)
+
+criaCartao(
+    'conhecimentos gerais',
+    'Qual é o maior planeta do sistema solar?',
+    'Júpiter: é o maior planeta do Sistema Solar, de acordo com a Nasa. Seu diâmetro é de 142 mil 984 quilômetros, o que significa que é aproximadamente 11 vezes maior do que a Terra.'
+)
+
+criaCartao(
+    'conhecimentos gerais',
+    'Quantos anos uma tartaruga vive?',
+'Quelônios como tartarugas, cágados e jabutis podem ultrapassar um século de vida. As maiores, como as tartarugas marinhas, costumam viver entre 80 e 100 anos, enquanto a tartaruga-gigante pode alcançar 200 anos ou mais.'
+)
+
+criaCartao(
+    'conhecimentos gerais',
+    'Por que sentimos coceira?',
+    'A coceira é uma reação do corpo para alertar sobre possíveis irritações na pele, como picadas de insetos ou substâncias irritantes. Coçar ajuda a remover a causa, mas também estimula o cérebro, liberando endorfinas'
+)
